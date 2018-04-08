c921bafab7ed3a167b98fd18c544eadd2660af5f
Juan Manuel Torres <kinojman@gmail.com>
2018-04-07 23:05:56 -0700

    Adding files for challenge

---
+++ Microb/Update/Function.cs
@@ -0,0 +1,19 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+
+namespace Microb.Update {
+    
+    //--- Methods ---
+    class Function {
+        
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public APIGatewayProxyResponse LambdaHandler(APIGatewayProxyRequest request) {
+            LambdaLogger.Log(JsonConvert.SerializeObject(request));
+            return new APIGatewayProxyResponse {
+                Body = "{\"title\": \"Hello API Gateway!\", \"content\": \"foo bar\"}",
+                StatusCode = 200
+            };
+        }
+    }
+}
---
+++ Microb/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+dotnet build
+
+#create deployment package
+dotnet lambda package --configuration release --framework netcoreapp2.0 --output-package bin/release/netcoreapp2.0/deploy-package.zip
+
