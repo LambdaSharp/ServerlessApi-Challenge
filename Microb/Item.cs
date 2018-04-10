8160166b7a2d6af3c097ecb727e32e72c73497b6
Juan Manuel Torres <kinojman@gmail.com>
2018-04-09 18:58:24 -0700

    New item class

---
+++ Microb/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace Microb {
+    public class MicrobItem {
+        public string title { get; set; }
+        public string content { get; set; }
+        public string date { get; set; }
+        public string id { get; set; }
+    } 
+}
